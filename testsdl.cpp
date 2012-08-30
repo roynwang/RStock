8447175b521a1868a8352beebd743b72e8461c6f
renywang <renywang@3cnd01702.(none)>
2012-08-30 16:19:19 +0800

    on master

---
+++ testsdl.cpp
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testsdl.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/30/2012 04:18:02 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+ // try to go to company
+#include <stdlib.h>
+
